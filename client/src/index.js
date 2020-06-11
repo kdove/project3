af73bb04bb146185731a67199f2453469e75b8e2
Pastor <randyvaca11@gmail.com>
2020-06-10 21:23:02 -0500

    R

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
-import ImageUpload from "./components/Upload/imageUpload";
-
-ReactDOM.render( < ImageUpload / > , document.getElementById("root"));
-registerServiceWorker();
